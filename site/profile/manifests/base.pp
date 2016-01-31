a691879b11ecc9a6d22aaf03d5847791fd14e7c1
Stephen Nesbitt <snesbitt@aussieswithtails.com>
2016-01-30 22:02:07 -0800

    Add base profil;e to default node. Add simple notify message into profile::base

--- site/profile/manifests/base.pp
+++ site/profile/manifests/base.pp
@@ -1,5 +1,7 @@
 class profile::base {
-
+  notify { 'profile::base':
+    message => 'Module ::profile::base'
+  }
   #the base profile should include component modules that will be on all nodes
 
 }
