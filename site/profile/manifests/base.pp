c9c68b9a128465fbee0685d40040c829400bc8d3
Stephen Nesbitt <snesbitt@aussieswithtails.com>
2016-02-06 16:24:52 -0800

    Renable ntp include

--- site/profile/manifests/base.pp
+++ site/profile/manifests/base.pp
@@ -3,6 +3,7 @@ class profile::base {
   notify { 'profile::base':
     message => 'Module ::profile::base',
   }
+  #
   #the base profile should include component modules that will be on all nodes
-#  include ::ntp
+  include ::ntp
 }
