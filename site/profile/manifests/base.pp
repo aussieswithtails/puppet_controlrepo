d0d357bbd32374b58f8c590b4d4239133fc1c052
Stephen Nesbitt <snesbitt@aussieswithtails.com>
2016-02-06 16:17:08 -0800

    Comment out include ::ntp for debugging purposes

--- site/profile/manifests/base.pp
+++ site/profile/manifests/base.pp
@@ -4,5 +4,5 @@ class profile::base {
     message => 'Module ::profile::base',
   }
   #the base profile should include component modules that will be on all nodes
-  include ::ntp
+#  include ::ntp
 }
