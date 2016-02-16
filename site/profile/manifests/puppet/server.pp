0902406614d174c4c3e1f018edd0fa517ae017cf
Stephen Nesbitt <snesbitt@aussieswithtails.com>
2016-02-15 16:09:27 -0800

    Add hiera.yaml template to puppetserver profile

---
+++ site/profile/manifests/puppet/server.pp
@@ -0,0 +1,9 @@
+class profile::puppet::server {
+  file {'hiera.yaml':
+    ensure  => file,
+    path    => "${settings::codedir}/hiera.yaml",
+    content => file('site/profile/hiera.yaml'),
+    mode    => '0644',
+  }
+
+}
