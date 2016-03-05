23dc1e501b2f7af8d035470845d02b31fb90f45c
Stephen Nesbitt <snesbitt@aussieswithtails.com>
2016-03-05 10:01:45 -0800

    Ensure puppet master profile is installed first

--- site/role/manifests/fuzzbuttnet_master_server.pp
+++ site/role/manifests/fuzzbuttnet_master_server.pp
@@ -6,8 +6,8 @@
 #
 #   include ::role::fuzzbuttnet_master_server
 class role::fuzzbuttnet_master_server {
-  include ::profile::puppet::master
-  include ::profile::ad_controller::backup_domain_controller
+  include profile::puppet::master
+  include profile::ad_controller::backup_domain_controller
 
-  Class['::profile::puppet::master'] -> Class['::profile::ad_controller::backup_domain_controller']
+  Class['profile::puppet::master'] -> Class['profile::ad_controller::backup_domain_controller']
 }
