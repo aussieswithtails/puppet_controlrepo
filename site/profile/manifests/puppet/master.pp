e9ebe42ad7bbbb634ed36e2d96fd859feeb35d8d
Stephen Nesbitt <snesbitt@aussieswithtails.com>
2016-03-05 12:59:21 -0800

    Checkpoint - Place hiera.yaml under puppet management

---
+++ site/profile/manifests/puppet/master.pp
@@ -0,0 +1,11 @@
+class profile::puppet::master {
+#  file {'hiera.yaml':
+#    ensure  => file,
+#    path    => "${settings::codedir}/hiera.yaml",
+#    content => file('profile/hiera.yaml'),
+#    mode    => '0644',
+#  }
+  include hiera
+  include ::puppetdb
+  include ::puppetdb::master::config
+}
--- site/profile/manifests/puppetmaster.pp
+++ site/profile/manifests/puppetmaster.pp
@@ -1,28 +1,28 @@
 class profile::puppetmaster {
 
   $hiera_yaml = "${::settings::confdir}/hiera.yaml"
-
-  class { 'hiera':
-    hierarchy  => [
-      'virtual/%{::virtual}',
-      'nodes/%{::trusted.certname}',
-      'common',
-    ],
-    hiera_yaml => $hiera_yaml,
-    datadir    => '/etc/puppetlabs/code/environments/%{environment}/hieradata',
-    owner      => 'pe-puppet',
-    group      => 'pe-puppet',
-    notify     => Service['pe-puppetserver'],
-  }
-
-  ini_setting { 'puppet.conf hiera_config main section' :
-    ensure  => present,
-    path    => "${::settings::confdir}/puppet.conf",
-    section => 'main',
-    setting => 'hiera_config',
-    value   => $hiera_yaml,
-    notify  => Service['pe-puppetserver'],
-  }
+  include hiera
+#  class { 'hiera':
+#    hierarchy  => [
+#      'virtual/%{::virtual}',
+#      'nodes/%{::trusted.certname}',
+#      'common',
+#    ],
+#    hiera_yaml => $hiera_yaml,
+#    datadir    => '/etc/puppetlabs/code/environments/%{environment}/hieradata',
+#    owner      => 'pe-puppet',
+#    group      => 'pe-puppet',
+#    notify     => Service['pe-puppetserver'],
+#  }
+#
+#  ini_setting { 'puppet.conf hiera_config main section' :
+#    ensure  => present,
+#    path    => "${::settings::confdir}/puppet.conf",
+#    section => 'main',
+#    setting => 'hiera_config',
+#    value   => $hiera_yaml,
+#    notify  => Service['pe-puppetserver'],
+#  }
 
   ini_setting { 'puppet.conf hiera_config master section' :
     ensure  => absent,
