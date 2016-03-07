5f25b328a3f01fd65abddc393953562310a6baec
Stephen Nesbitt <snesbitt@aussieswithtails.com>
2016-03-07 09:51:25 -0800

    Checkpoint - Btrfs subvolume creation and mount to support vms

--- site/profile/manifests/vmhost.pp
+++ site/profile/manifests/vmhost.pp
@@ -8,41 +8,44 @@
 #   include ::profile::vmhost
 #
 class profile::vmhost {
-#  $btrfs_host_volume = hiera('btrfs_device')
-#  $btrfs_admin_mountpoint = hiera('btrfs_admin_mountpoint')
-#  $btrfs_vms_subvolume = '@vms'
-#  $vms_mountpoint = hiera('vms_repo')
-#  $btrfs_subvolume_path = "${btrfs_admin_mountpoint}/${btrfs_vms_subvolume}"
-#
+  $btrfs_host_volume = hiera('btrfs_device')
+  $btrfs_admin_mountpoint = '/mnt/btrfs'
+  $btrfs_vms_subvolume = '@vms'
+  $vms_mountpoint = '/var/lib/vms'
+  $btrfs_subvolume_path = "${btrfs_admin_mountpoint}/${btrfs_vms_subvolume}"
+
 #  include ::vagrant
 #  include ::virtualbox
-#
-#
-#  mkdir::p { 'btrfs_admin_mount_point':
-#    path    => $btrfs_admin_mountpoint,
-#  }
-#
-#  mkdir::p { 'vms_repo':
-#    path    => $vms_mountpoint,
-#  }
-#
-#  ::subvolume { $btrfs_subvolume_path:
-#    ensure  => present,
-#    require => Mount[$btrfs_admin_mountpoint],
-#  }
-#
-#  mount { $btrfs_admin_mountpoint:
-#    ensure => mounted,
-#    device => $btrfs_host_volume,
-#    fstype => 'btrfs',
-#  }
-#
-#  mount { $vms_mountpoint:
-#    ensure  => mounted,
-#    device  => $btrfs_host_volume,
-#    fstype  => 'btrfs',
-#    options => "defaults,subvol=${btrfs_vms_subvolume}",
-#    pass    => 2,
-#    require => Subvolume[$btrfs_subvolume_path],
-#  }
+
+  # Create a vms subvolume
+  mkdir::p { 'btrfs_admin_mount_point':
+    path    => $btrfs_admin_mountpoint,
+    before  => Mount[$btrfs_admin_mountpoint],
+  }
+
+  mount { $btrfs_admin_mountpoint:
+    ensure => mounted,
+    device => $btrfs_host_volume,
+    fstype => 'btrfs',
+    before  => Subvolume[$btrfs_subvolume_path]
+  }
+
+  subvolume { $btrfs_subvolume_path: #FixMe - I don't like syntax here. Should be a parameter that specifies
+    ensure  => present,              # path to where btrfs is mounted
+  }
+
+  # Create the vms mount point and mount the btrfs vms subvolume
+  mkdir::p { 'vms_repo':
+    path    => $vms_mountpoint,
+    before  => Mount[$vms_mountpoint]
+  }
+
+  mount { $vms_mountpoint:
+    ensure  => mounted,
+    device  => $btrfs_host_volume,
+    fstype  => 'btrfs',
+    options => "defaults,subvol=${btrfs_vms_subvolume}",
+    pass    => 2,
+    require => Subvolume[$btrfs_vms_path]
+  }
 }
---
+++ utilities/aliases.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+alias pagent='sudo /opt/puppetlabs/bin/puppet agent --environment=development --test'
+alias papply='sudo /opt/puppetlabs/bin/puppet apply --environment=development --test'
