90cd9bac8e645a3c7818c2270db307a62eb2992a
Stephen Nesbitt <snesbitt@aussieswithtails.com>
2016-03-10 11:23:53 -0800

    Checkpoint - refactor

--- site/profile/manifests/vagrant_server.pp
+++ site/profile/manifests/vagrant_server.pp
@@ -1,14 +1,14 @@
 class profile::vagrant_server {
   # create a btrfs subvolume to host the vagrant repository and
   # mount it at /var/www/<vagrant>
-  $www_subvolume = '@www'
-  $vagrant_subvolume = 'vagrant'
+  $btrfs_admin_mountpoint = '/mnt/btrfs'
+  $www_subvolume_id = '@www'
+  $vagrant_subvolume_id = 'vagrant'
   $vagrant_repo_mountpoint = '/var/lib/www/vagrant'
   $btrfs_host_volume = hiera('btrfs_device')  #ToDo - Currently lack of value causes failure of catalog.
-  #Instead lack of value should result in failure of this profile only!
-  $btrfs_admin_mountpoint = '/mnt/btrfs'
-  $btrfs_www_subvolume = "${btrfs_admin_mountpoint}/${www_subvolume}"
-  $btrfs_vagrant_subvolume = "${btrfs_www_subvolume}/$vagrant_subvolume"
+
+#  $btrfs_www_subvolume_path = "${btrfs_admin_mountpoint}/${www_subvolume_id}"
+#  $btrfs_vagrant_subvolume = "${btrfs_www_subvolume_path}/$vagrant_subvolume_id"
 
 
   # FixMe - this entire process of creating and mounting a btrfs subvolume should be put in a module
@@ -24,14 +24,14 @@ class profile::vagrant_server {
     require => Mkdir::P['btrfs_admin_mount_point'],
   }
 
-  subvolume { "${btrfs_admin_mountpoint}/${www_subvolume}": #FixMe - I don't like syntax here. Should be a parameter that specifies
+  subvolume { "${btrfs_admin_mountpoint}/${www_subvolume_id}": #FixMe - I don't like syntax here. Should be a parameter that specifies
     ensure  => present,              # path to where btrfs is mounted
     require => Mount[$btrfs_admin_mountpoint]
   }
 
-  subvolume { "${btrfs_admin_mountpoint}/${www_subvolume}/${vagrant_subvolume}": #FixMe - I don't like syntax here. Should be a parameter that specifies
+  subvolume { "${btrfs_admin_mountpoint}/${www_subvolume_id}/${vagrant_subvolume_id}": #FixMe - I don't like syntax here. Should be a parameter that specifies
     ensure   => present,              # path to where btrfs is mounted
-    require  => Subvolume["${btrfs_admin_mountpoint}/${www_subvolume}"]
+    require  => Subvolume["${btrfs_admin_mountpoint}/${www_subvolume_id}"]
   }
 
     # Create the vms mount point and mount the btrfs vms subvolume
@@ -44,9 +44,9 @@ class profile::vagrant_server {
     ensure  => mounted,
     device  => $btrfs_host_volume,
     fstype  => 'btrfs',
-    options => "defaults,subvol=${www_subvolume}/${vagrant_subvolume}",
+    options => "defaults,subvol=${www_subvolume_id}/${vagrant_subvolume_id}",
     pass    => 2,
-    require => Subvolume["${btrfs_admin_mountpoint}/${www_subvolume}/${vagrant_subvolume}"],
+    require => Subvolume["${btrfs_admin_mountpoint}/${www_subvolume_id}/${vagrant_subvolume_id}"],
   }
 #   add an nginx server
 
