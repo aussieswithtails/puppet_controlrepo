9b3d0be5995a5799cd29b0c0b53d77f1851f223c
Stephen Nesbitt <snesbitt@aussieswithtails.com>
2016-03-21 19:19:20 -0700

    Fix key issues

--- site/profile/manifests/git/gitolite.pp
+++ site/profile/manifests/git/gitolite.pp
@@ -14,17 +14,17 @@ class profile::git::gitolite (
   user { $gitolite_server_owner:
     ensure     => present,
     home       => $gitolite_mountpoint,
-    managehome => true,
+    managehome => false,
     shell      => '/bin/bash',
     system     => true,
   }
 
-  ssh_keygen { $gitolite_server_owner:
-    bits    => hiera('awt::ssh::key_size'),
-    home    => $gitolite_mountpoint,
-    type    => hiera('awt::ssh::key_type'),
-    require => Profile::Types::File_and_mount[$gitolite_mountpoint],
-  }
+  # ssh_keygen { $gitolite_server_owner:
+  #   bits    => hiera('awt::ssh::key_size'),
+  #   home    => $gitolite_mountpoint,
+  #   type    => hiera('awt::ssh::key_type'),
+  #   require => Profile::Types::File_and_mount[$gitolite_mountpoint],
+  # }
 
   ensure_resource('profile::types::file_and_mount', $btrfs_admin_mountpoint, {
     file_params  => { },
@@ -39,7 +39,7 @@ class profile::git::gitolite (
 
   subvolume { $btrfs_subvolume_path: #FixMe - I don't like syntax here. Should be a parameter that specifies
     ensure  => present,              # path to where btrfs is mounted
-    require => Profile::Types::File_and_mount[$btrfs_admin_mountpoint],
+    require => [Profile::Types::File_and_mount[$btrfs_admin_mountpoint],User[$gitolite_server_owner]]
   }
 
 
@@ -56,5 +56,6 @@ class profile::git::gitolite (
       'options' => "defaults,subvol=${btrfs_gitolite_subvolume_id}",
     },
     require      => [User[$gitolite_server_owner], Subvolume[$btrfs_subvolume_path]],
+    before       => Class['Gitolite']
   }
 }
--- site/role/manifests/fuzzbuttnet_master_server.pp
+++ site/role/manifests/fuzzbuttnet_master_server.pp
@@ -7,5 +7,6 @@
 #   include ::role::fuzzbuttnet_master_server
 class role::fuzzbuttnet_master_server {
   include profile::puppet::master
-  include profile::ad_controller::backup_domain_controller
+  # include profile::ad_controller::backup_domain_controller
+  include profile::git::gitolite
 }
